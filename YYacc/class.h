756815cea3727adf0adcdbb28ee27bd85c9e7e43
CyanideCentral <tetracyanoethylene@163.com>
2017-11-21 20:10:56 +0800

    Closure impl Use lambda to compare states.

--- YYacc/class.h
+++ YYacc/class.h
@@ -1,6 +1,7 @@
 #pragma once
 #include<iostream>
 #include<string>
+#include<algorithm>
 #include<unordered_set>
 #include<unordered_map>
 #include<vector>
@@ -12,13 +13,14 @@ public:
 	string left;
 	vector<string>* right;
 	int dot;
-	string la;
+	unordered_set<string>* la;
 
 	Production(string leftNT);
 	~Production();
 	bool equalTo(Production* p);
 	bool equalCore(Production* p);
 	Production* extendRight(string symbol);
+	Production* clone();
 };
 
 class State {
@@ -29,14 +31,22 @@ public:
 
 	State(int num);
 	~State();
+	bool equalTo(vector<Production*>* ns);
+	bool insert(Production* prod, int dot = -1, string la = "");
 };
 
 class Analyzer {
 private:
 	vector<State*>* states;
 	vector<Production*>* grammar;
+	vector<string>* tokens;
+	//Map from non-terminal to its productions
+	unordered_map<string, vector<int>*>* prodmap;
+
+	void toClosure(State* st);
+	bool isTerminal(string name);
 public:
-	vector<unordered_map<string, int>*> toParsingTable(vector<Production*>* augGrammar);
+	vector<unordered_map<string, int>*> toParsingTable(vector<Production*>* augGrammar, vector<string>* tokenList);
 
 	Analyzer();
 	~Analyzer();
--- YYacc/lr.cpp
+++ YYacc/lr.cpp
@@ -3,18 +3,19 @@
 Production::Production(string leftNT) {
 	left = leftNT;
 	right = new vector<string>;
-	la = "";
+	la = new unordered_set<string>;
 	dot = 0;
 }
 
 Production::~Production() {
 	delete right;
+	delete la;
 }
 
 bool Production::equalTo(Production * p) {
 	if (!equalCore(p)) return false;
-	for (int i = 0; i < la.size(); i++) {
-		if (p->la.find_first_of(la[i]) == string::npos) return false;
+	for (unordered_set<string>::iterator it = la->begin(); it != la->end(); it++) {
+		if (find(p->la->begin(), p->la->end(), *it) == p->la->end()) return false;
 	}
 	return true;
 }
@@ -34,6 +35,16 @@ Production * Production::extendRight(string symbol) {
 	return this;
 }
 
+Production * Production::clone() {
+	Production* copy = new Production(left);
+	copy->dot = dot;
+	copy->la->insert(la->begin(),la->end());
+	for (int i = 0; i < right->size(); i++) {
+		copy->right->push_back((*right)[i]);
+	}
+	return copy;
+}
+
 State::State(int num) {
 	id = num;
 	prods = new vector<Production*>;
@@ -45,15 +56,91 @@ State::~State() {
 	delete edges;
 }
 
-vector<unordered_map<string, int>*> Analyzer::toParsingTable(vector<Production*>* augGrammar) {
+bool State::equalTo(vector<Production*>* ns) {
+	if (prods->size() != ns->size()) return false;
+	for (int i = 0; i < ns->size(); i++) {
+		//If no same production is found in this state
+		if (none_of(prods->begin(), prods->end(), [ns, i](Production* p) {return ns->at(i)->equalTo(p); })) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool State::insert(Production * prod, int dot, string la) {
+	Production * np = new Production(prod->left);
+	for (int i = 0; i < prod->right->size(); i++) {
+
+	}
+	return false;
+}
+
+void Analyzer::toClosure(State * st) {
+	unordered_map<string, unordered_set<string>*>* lamap = new unordered_map<string, unordered_set<string>*>;
+	for (int i = 0; i < st->prods->size(); i++) {
+		Production* pr = (*(st->prods))[i];
+		//calculate First()
+		if (pr->dot < pr->right->size() && !isTerminal(pr->right->at(pr->dot))) {
+			if (lamap->find(pr->left) == lamap->end()) (*lamap)[pr->left] = new unordered_set<string>;
+			if (pr->dot+1 == pr->right->size()) {
+				(*lamap)[pr->left]->insert(pr->la->begin(), pr->la->end());
+			}
+			else {
+				(*lamap)[pr->left]->insert(pr->right->at(pr->dot + 1));
+			}
+		}
+	}
+	for (unordered_map<string, unordered_set<string>*>::iterator it = lamap->begin(); it != lamap->end(); it++) {
+		vector<int>* head = prodmap->at(it->first);
+		for (int j = 0; j < head->size(); j++) {
+			Production* pr = grammar->at(head->at(j));
+			bool found = false;
+			for (int k = 0; k < st->prods->size(); k++) {
+				//Same production found, add lookaheads
+				if ((*(st->prods))[k]->equalCore(pr)) {
+					(*(st->prods))[k]->la->insert(it->second->begin(), it->second->end());
+					found = true;
+					break;
+				}
+			}
+			//Insert production to this state
+			if (!found) {
+				Production* copy = pr->clone();
+				copy->la->insert(it->second->begin(), it->second->end());
+				st->prods->push_back(copy);
+			}
+		}
+	}
+	delete lamap;
+}
+
+bool Analyzer::isTerminal(string name) {
+	return find(tokens->begin(), tokens->end(), name) != tokens->end();
+}
+
+vector<unordered_map<string, int>*> Analyzer::toParsingTable(vector<Production*>* augGrammar, vector<string>* tokenList) {
 	grammar = augGrammar;
+	tokens = tokenList;
+
+	for (int i = 0; i < tokenList->size(); i++) {
+		Production* p = (*grammar)[i];
+		if (prodmap->find(p->left) != prodmap->end()) {
+			(*prodmap)[p->left]->push_back(i);
+		}
+		else {
+			(*prodmap)[p->left] = new vector<int>;
+			(*prodmap)[p->left]->push_back(i);
+		}
+	}
 }
 
 Analyzer::Analyzer() {
 	states = new vector<State*>;
+	prodmap = new unordered_map<string, vector<int>*>;
 	grammar = NULL;
 }
 
 Analyzer::~Analyzer() {
 	delete states;
+	delete prodmap;
 }
